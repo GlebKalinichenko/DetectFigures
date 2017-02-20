2985ba9e7d7bb8c463b3f198f50bdf371ea747f8
GlebKalinichenko <Glebjn@yandex.ua>
2017-02-20 11:32:06 +0000

    Initial commint

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+import java.awt.image.BufferedImage;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    BitmapHelper helper = BitmapHelper.getInstance();
+        BufferedImage img = helper.getImage("C:\\Users\\Gleb\\IdeaProjects\\DetectFigures\\name.png");
+        int[][] binaryPixels = helper.getPixels(img);
+        helper.getNumObjects(binaryPixels);
+        helper.getMarkers(binaryPixels);
+    }
+}
